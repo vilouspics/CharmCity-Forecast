627dc993d8e96b8f79027ea9a9af78ab3ac72209
Victoria Louise Steinwachs <69801019+vilouspics@users.noreply.github.com>
2024-01-28 16:21:51 +0100

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,43 @@
 function refreshWeather(response) {
     let temperatureElement = document. querySelector("#current-temperature-value");
     let temperature = response.data.temperature.current;
-    temperatureElement.innerHTML = Math.round(temperature);
     let cityElement = document.querySelector("#current-city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let dayTimeElement = document.querySelector("#day-and-time");
+    let date = new Date(response.data.time * 1000);
+
+    console.log(response.data);
+    
     cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    dayTimeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+    
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+      ];
+      let day = days[date.getDay()];
+
+      if (minutes < 10) {
+        minutes = `0${minutes}`;
+      }
+
+      return `${day} ${hours}:${minutes}, `;
 }
 
 function searchCity(city) {
